75cdf4a8c495b0a7af1ff76d51a87355b8849116
apbank2238 <bayulgang@hotmail.com>
2025-04-09 15:23:19 +0700

    add new function that use env

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,11 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post()
+  login(@Body() body: { username: string; password: string }) {
+    const { username, password } = body;
+    return this.appService.login(username, password);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,12 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  login(username: string, password: string): string {
+    if (username !== 'admin' || password !== process.env.PASSWORD) {
+      return 'Login failed';
+    }
+
+    return 'Login success';
+  }
 }
